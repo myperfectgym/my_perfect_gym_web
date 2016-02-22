b1446ca6208d55ea0a207a445d5b5fee5995c51f
Vyacheslav <vyacheslavkarmazin@gmail.com>
2016-02-22 21:04:10 +0300

    templates

---
+++ frontend/assets/JqueryQIAssets.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class JqueryQIAssets extends AssetBundle
+{
+    public $basePath = '@webroot/plugins';
+    public $baseUrl = '@web/plugins';
+    public $css = [
+    ];
+    public $js = [
+        'jquery-ui/jquery-ui.min.js',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+    ];
+}
---
+++ frontend/assets/Select2Asset.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class Select2Asset extends AssetBundle
+{
+    public $sourcePath = '@bower/select2';
+
+    public $css = [
+        'dist/css/select2.css',
+    ];
+    public $js = [
+        'dist/js/select2.min.js',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+    ];
+}
